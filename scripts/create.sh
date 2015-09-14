2e198c8a8f9958c97b8833797ac23dde8db0e8b0
kemiz <kemimeister@gmail.com>
2015-09-14 18:06:51 +0100

    correct runtime props

--- scripts/create.sh
+++ scripts/create.sh
@@ -14,5 +14,5 @@ ctx logger info "Installing pyyaml"
 pip install pyyaml==3.10
 ctx logger info "Installing cloud-config-service"
 echo ${SOURCE}
-pip install --upgrade ${SOURCE}
+pip install --force-reinstall ${SOURCE}
 
